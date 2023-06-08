da76b886ce3f895c124fea623026d2910d7268fb
imtiaz246 <imtiazuddincho001@gmail.com>
2023-06-08 23:36:52 +0600

    Auth: session cache locking system & csrf middleware
    Signed-off-by: imtiaz246 <imtiazuddincho001@gmail.com>

--- app/router/routes/routes.go
+++ app/router/routes/routes.go
@@ -2,18 +2,35 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/csrf"
+	"github.com/gofiber/fiber/v2/middleware/helmet"
+	"github.com/gofiber/fiber/v2/utils"
 	"github.com/imtiaz246/codera_oj/app/handler"
 	"github.com/imtiaz246/codera_oj/services/middlewares"
+	"time"
 )
 
 func RegisterRoutes(app *fiber.App, handler *handler.Handler) {
 	apiV1 := app.Group("/api/v1")
+	app.Use(csrf.New(csrf.Config{
+		KeyLookup:      "header:X-Csrf-Token",
+		CookieName:     "csrf_",
+		CookieSameSite: "Lax",
+		Expiration:     1 * time.Hour,
+		KeyGenerator:   utils.UUID,
+		Storage:        storage,
+		//CookieHTTPOnly: true,
+		//CookieSecure:   true,
+	}))
+	app.Use(helmet.New())
+	//app.Use(limiter.New())
 
 	/* -------------------- Auth Routes Begins -------------------- */
 	auth := apiV1.Group("/auth")
+	auth.Use(csrf.New())
 	auth.Post("/signup", handler.SignUp)
 	auth.Post("/login", handler.Login)
-	auth.Get("renew-token", handler.RenewToken)
+	auth.Get("/renew-token", handler.RenewToken)
 	auth.Get("/verify-email/:id/:token", handler.VerifyEmail)
 	/* -------------------- Auth Routes Ends -------------------- */
 
